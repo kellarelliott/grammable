0c9d4fc842e77bf4aba4124e2190917c8e37eeba
Kellar Elliott <kellarelliott@gmail.com>
2019-11-07 23:26:40 +0000

    production.rb fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url)ptions = { host: 'https://grammable-kellar-elliott.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://grammable-kellar-elliott.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
